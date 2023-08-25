90f14aeefb063910a9d19b48529ba7b1d1fbea5f
Andre Bruwer <victorbruwer@gmail.com>
2023-08-25 09:02:47 +0200

    Efficient Image hashing and similarity indexing

--- Image Similarity indexer/MainForm.Designer.cs
+++ Image Similarity indexer/MainForm.Designer.cs
@@ -30,14 +30,28 @@
         {
             tabControl1 = new TabControl();
             tabPageDetails = new TabPage();
+            progressBar2 = new ProgressBar();
+            progressBar1 = new ProgressBar();
+            button1 = new Button();
             labelFolderDetails = new Label();
             label2 = new Label();
             buttonSelectFolder = new Button();
             checkBoxAnalyseSubfolder = new CheckBox();
             label1 = new Label();
             tabPageComparison = new TabPage();
+            labelIndexDetails = new Label();
+            buttonNext = new Button();
+            buttonPrevious = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabelElapsedMiliseconds = new ToolStripStatusLabel();
             tabControl1.SuspendLayout();
             tabPageDetails.SuspendLayout();
+            tabPageComparison.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -48,11 +62,14 @@
             tabControl1.Location = new Point(0, 0);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(974, 609);
+            tabControl1.Size = new Size(1329, 637);
             tabControl1.TabIndex = 0;
             // 
             // tabPageDetails
             // 
+            tabPageDetails.Controls.Add(progressBar2);
+            tabPageDetails.Controls.Add(progressBar1);
+            tabPageDetails.Controls.Add(button1);
             tabPageDetails.Controls.Add(labelFolderDetails);
             tabPageDetails.Controls.Add(label2);
             tabPageDetails.Controls.Add(buttonSelectFolder);
@@ -61,10 +78,34 @@
             tabPageDetails.Location = new Point(4, 24);
             tabPageDetails.Name = "tabPageDetails";
             tabPageDetails.Padding = new Padding(3);
-            tabPageDetails.Size = new Size(966, 581);
+            tabPageDetails.Size = new Size(1321, 609);
             tabPageDetails.TabIndex = 0;
             tabPageDetails.Text = "Details";
             tabPageDetails.UseVisualStyleBackColor = true;
+            // 
+            // progressBar2
+            // 
+            progressBar2.Location = new Point(10, 464);
+            progressBar2.Name = "progressBar2";
+            progressBar2.Size = new Size(393, 34);
+            progressBar2.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(10, 424);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(393, 34);
+            progressBar1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(10, 379);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // labelFolderDetails
             // 
@@ -115,19 +156,87 @@
             // 
             // tabPageComparison
             // 
+            tabPageComparison.Controls.Add(labelIndexDetails);
+            tabPageComparison.Controls.Add(buttonNext);
+            tabPageComparison.Controls.Add(buttonPrevious);
+            tabPageComparison.Controls.Add(pictureBox2);
+            tabPageComparison.Controls.Add(pictureBox1);
             tabPageComparison.Location = new Point(4, 24);
             tabPageComparison.Name = "tabPageComparison";
             tabPageComparison.Padding = new Padding(3);
-            tabPageComparison.Size = new Size(966, 581);
+            tabPageComparison.Size = new Size(1321, 609);
             tabPageComparison.TabIndex = 1;
             tabPageComparison.Text = "Comparison";
             tabPageComparison.UseVisualStyleBackColor = true;
+            // 
+            // labelIndexDetails
+            // 
+            labelIndexDetails.AutoSize = true;
+            labelIndexDetails.Location = new Point(419, 18);
+            labelIndexDetails.Name = "labelIndexDetails";
+            labelIndexDetails.Size = new Size(38, 15);
+            labelIndexDetails.TabIndex = 4;
+            labelIndexDetails.Text = "000 %";
+            // 
+            // buttonNext
+            // 
+            buttonNext.Location = new Point(844, 392);
+            buttonNext.Name = "buttonNext";
+            buttonNext.Size = new Size(75, 23);
+            buttonNext.TabIndex = 3;
+            buttonNext.Text = "Next";
+            buttonNext.UseVisualStyleBackColor = true;
+            buttonNext.Click += buttonNext_Click;
+            // 
+            // buttonPrevious
+            // 
+            buttonPrevious.Location = new Point(3, 392);
+            buttonPrevious.Name = "buttonPrevious";
+            buttonPrevious.Size = new Size(75, 23);
+            buttonPrevious.TabIndex = 2;
+            buttonPrevious.Text = "Previous";
+            buttonPrevious.UseVisualStyleBackColor = true;
+            buttonPrevious.Click += buttonPrevious_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox2.Location = new Point(547, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(372, 383);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(372, 383);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabelElapsedMiliseconds });
+            statusStrip1.Location = new Point(0, 615);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1329, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelElapsedMiliseconds
+            // 
+            toolStripStatusLabelElapsedMiliseconds.Name = "toolStripStatusLabelElapsedMiliseconds";
+            toolStripStatusLabelElapsedMiliseconds.Size = new Size(32, 17);
+            toolStripStatusLabelElapsedMiliseconds.Text = "0 ms";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(974, 609);
+            ClientSize = new Size(1329, 637);
+            Controls.Add(statusStrip1);
             Controls.Add(tabControl1);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
@@ -136,7 +245,14 @@
             tabControl1.ResumeLayout(false);
             tabPageDetails.ResumeLayout(false);
             tabPageDetails.PerformLayout();
+            tabPageComparison.ResumeLayout(false);
+            tabPageComparison.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -149,5 +265,15 @@
         private Button buttonSelectFolder;
         private CheckBox checkBoxAnalyseSubfolder;
         private Label label1;
+        private Button button1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabelElapsedMiliseconds;
+        private ProgressBar progressBar1;
+        private ProgressBar progressBar2;
+        private Button buttonNext;
+        private Button buttonPrevious;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label labelIndexDetails;
     }
 }
--- Image Similarity indexer/MainForm.cs
+++ Image Similarity indexer/MainForm.cs
@@ -1,34 +1,55 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+using Codeuctivity.ImageSharpCompare;
+using System.Windows.Forms;
+using MathNet.Numerics;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System.Drawing.Text;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Image_Similarity_indexer
 {
     public partial class MainForm : Form
     {
-        List<FileInfo> imageFiles = new List<FileInfo>();
+        List<ImageHash> imageHashes = new List<ImageHash>();
+        List<ImageSimilarityIndex> imageSimilarityIndices = new List<ImageSimilarityIndex>();
+        StopwatchElement stopwatch;
+        int displayedIndex = 0;
+
         public MainForm()
         {
             InitializeComponent();
+            stopwatch = new StopwatchElement(toolStripStatusLabelElapsedMiliseconds);
         }
 
         private void buttonSelectFolder_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
             {
-                Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
 
                 //make the list of images to analyse in the folder
-                imageFiles.Clear();
+                List<FileInfo> imageFiles = new List<FileInfo>();
                 imageFiles = GetAllImageInFolder(folderBrowserDialog.SelectedPath, checkBoxAnalyseSubfolder.Checked);
 
                 //group the images by extention and show how many of each there are
                 var groupedFiles = imageFiles.GroupBy(file => file.Extension.ToLower());
 
+                foreach (FileInfo file in imageFiles)
+                {
+                    imageHashes.Add(new ImageHash { imagePath = file.FullName });
+                }
+
                 stopwatch.Stop();
-                labelFolderDetails.Text = $"{stopwatch.ElapsedMilliseconds} ms\n{imageFiles.Count} image(s) in total\n{GroupsByExtention(groupedFiles)}";
+                labelFolderDetails.Text = $"{imageFiles.Count} image(s) in total\n{GroupsByExtention(groupedFiles)}";
             }
         }
 
@@ -44,7 +65,7 @@ namespace Image_Similarity_indexer
             return groupings;
         }
 
-        private List<FileInfo> GetAllImageInFolder(string folderPath, bool searchSubfolders) 
+        private List<FileInfo> GetAllImageInFolder(string folderPath, bool searchSubfolders)
         {
             DirectoryInfo directory = new DirectoryInfo(folderPath);
             return directory.GetFiles("*.*", searchSubfolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
@@ -57,5 +78,322 @@ namespace Image_Similarity_indexer
             return imageExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase);
         }
 
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stopwatch.Start();
+
+                //calculate hash for all images in imageHashes
+                if (imageHashes.Count == 0)
+                    throw new Exception("No images were selected");
+
+                int pixelsWide = 50;
+
+                progressBar1.Value = 0;
+                progressBar1.Maximum = imageHashes.Count;
+
+                await Task.Run(() =>
+                {
+                    Parallel.ForEach(
+                        imageHashes,
+                        new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+                        imageHash =>
+                    {
+                        ImageToHash(ref imageHash, pixelsWide);
+                        progressBar1.Invoke(() =>
+                        {
+                            progressBar1.Value += 1;
+                        });
+                    });
+                });
+
+                imageSimilarityIndices = new List<ImageSimilarityIndex>();
+
+                for (int i = 0; i < imageHashes.Count; i++)
+                {
+                    for (int j = i + 1; j < imageHashes.Count; j++)
+                    {
+                        imageSimilarityIndices.Add(new ImageSimilarityIndex()
+                        {
+                            imageHash1 = imageHashes[i],
+                            imageHash2 = imageHashes[j]
+                        });
+                    }
+                }
+
+                progressBar2.Value = 0;
+                progressBar2.Maximum = imageSimilarityIndices.Count;
+
+                await Task.Run(() =>
+                {
+                    Parallel.ForEach(
+                        imageSimilarityIndices,
+                        new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+                        imageSimilarityIndex =>
+                        {
+                            CalculateHashSimilarity(ref imageSimilarityIndex);
+                            progressBar2.Invoke(() =>
+                            {
+                                progressBar2.Value += 1;
+                            });
+                        });
+                });
+
+                imageSimilarityIndices = imageSimilarityIndices.OrderByDescending(x => x.similarity).ToList();
+
+                stopwatch.Stop();
+
+                DisplayImagesOfIndex();
+
+                //MessageBox.Show($"{stopwatch.ElapsedMiliseconds} ms");
+
+
+
+
+
+                //ImageSimilarityIndex imageSimilarityIndex = new ImageSimilarityIndex()
+                //{
+                //    imageHash1 = new ImageHash()
+                //    {
+                //        imagePath = @"D:\My Pictures\Wallpapers\__original_drawn_by_kazami_kuroro__sample-bfa02104b3b48ba3c485e0b85309c35e.jpg"
+                //    },
+                //    imageHash2 = new ImageHash()
+                //    {
+                //        imagePath = @"D:\My Pictures\Wallpapers\6f66681b78ce2287fc60effcc04805bf.jpg"
+                //    }
+                //};
+
+                //
+
+
+                //ImageToHash(ref imageSimilarityIndex.imageHash1, pixelsWide);
+                //ImageToHash(ref imageSimilarityIndex.imageHash2, pixelsWide);
+
+                ////compare image hashes
+                //imageSimilarityIndex.similarity = CalculateHashSimilarity(imageSimilarityIndex.imageHash1.hash, imageSimilarityIndex.imageHash2.hash);
+
+                //
+                //imageSimilarityIndex.elapsedMS = stopwatch.ElapsedMiliseconds;
+
+                //MessageBox.Show($"{imageSimilarityIndex.similarity.ToString("0.000")}%\n{imageSimilarityIndex.elapsedMS} ms");
+            }
+            catch (Exception ex)
+            {
+                stopwatch.Stop();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ImageToHash(ref ImageHash imageHash, int resolution)
+        {
+            imageHash.resolution = resolution;
+            //load in the image
+
+            Bitmap image1 = new Bitmap(imageHash.imagePath);
+
+            //resize the images
+            Bitmap imageSmaller = new Bitmap(image1, resolution, resolution);
+
+            //greyscale the images
+            Bitmap imageGrey = MakeGrayscale(imageSmaller);
+
+            //hash the image
+            imageHash.hash = MakeHashedString(imageGrey);
+
+
+            //clean momory because it could overwhelm the RAM
+            image1.Dispose();
+            imageSmaller.Dispose();
+            imageGrey.Dispose();
+            //GC.Collect();
+        }
+
+        private void CalculateHashSimilarity(ref ImageSimilarityIndex imageSimilarityIndex)
+        {
+            double length = imageSimilarityIndex.imageHash1.hash.Length;
+            double matches = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                char c1 = imageSimilarityIndex.imageHash1.hash[i];
+                char c2 = imageSimilarityIndex.imageHash2.hash[i];
+
+                if (c1 == c2)
+                {
+                    matches++;
+                }
+            }
+
+            imageSimilarityIndex.similarity = matches / length * 100;
+        }
+        private void DisplayImagesOfIndex()
+        {
+            if (imageSimilarityIndices.Count == 0)
+            {
+                pictureBox1.BackgroundImage = pictureBox2.BackgroundImage = null;
+                labelIndexDetails.ResetText();
+                return;
+            }
+
+            if (displayedIndex < 0)
+            {
+                displayedIndex = imageSimilarityIndices.Count - 1;
+            }
+            else if (displayedIndex >= imageSimilarityIndices.Count)
+            {
+                displayedIndex = 0;
+            }
+
+            pictureBox1.BackgroundImage = new Bitmap(imageSimilarityIndices[displayedIndex].imageHash1.imagePath);
+            pictureBox2.BackgroundImage = new Bitmap(imageSimilarityIndices[displayedIndex].imageHash2.imagePath);
+            labelIndexDetails.Text = $"{imageSimilarityIndices[displayedIndex].similarity.ToString("0.000")} %";
+        }
+
+        private string MakeHashedString(Bitmap original)
+        {
+            string hashValue = "";
+
+            for (int i = 0; i < original.Height - 1; i++)
+            {
+                for (int j = 0; j < original.Width - 1; j++)
+                {
+                    int pixel1 = original.GetPixel(i, j).R;
+                    int pixel2 = original.GetPixel(i, j + 1).R;
+
+                    if (pixel1 >= pixel2)
+                    {
+                        hashValue += "1";
+                    }
+                    else
+                    {
+                        hashValue += "0";
+                    }
+                }
+            }
+            return hashValue;
+        }
+
+        private Bitmap MakeHashedImage(Bitmap original)
+        {
+            Bitmap newBitmap = new Bitmap(original.Width - 1, original.Height - 1);
+
+            for (int i = 0; i < newBitmap.Height; i++)
+            {
+                for (int j = 0; j < newBitmap.Width; j++)
+                {
+                    int pixel1 = original.GetPixel(i, j).R;
+                    int pixel2 = original.GetPixel(i, j + 1).R;
+
+                    if (pixel1 >= pixel2)
+                    {
+                        newBitmap.SetPixel(i, j, Color.White);
+                    }
+                    else
+                    {
+                        newBitmap.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+            return newBitmap;
+        }
+
+        public static Bitmap MakeGrayscale(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            using (Graphics g = Graphics.FromImage(newBitmap))
+            {
+
+                //create the grayscale ColorMatrix
+                ColorMatrix colorMatrix = new ColorMatrix(
+                   new float[][]
+                   {
+             new float[] {.3f, .3f, .3f, 0, 0},
+             new float[] {.59f, .59f, .59f, 0, 0},
+             new float[] {.11f, .11f, .11f, 0, 0},
+             new float[] {0, 0, 0, 1, 0},
+             new float[] {0, 0, 0, 0, 1}
+                   });
+
+                //create some image attributes
+                using (ImageAttributes attributes = new ImageAttributes())
+                {
+                    //set the color matrix attribute
+                    attributes.SetColorMatrix(colorMatrix);
+
+                    //draw the original image on the new image
+                    //using the grayscale color matrix
+                    g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+                                0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+                }
+            }
+            return newBitmap;
+        }
+
+
+        partial class ImageSimilarityIndex
+        {
+            public ImageHash imageHash1,
+                imageHash2;
+            public double similarity;
+        }
+        partial class ImageHash
+        {
+            public string imagePath,
+                hash;
+            public int resolution;
+        }
+        partial class StopwatchElement
+        {
+            private Stopwatch? timer;
+            ToolStripLabel toolStripLabel;
+
+            public StopwatchElement(ToolStripLabel toolStripLabel)
+            {
+                timer = new Stopwatch();
+                this.toolStripLabel = toolStripLabel;
+            }
+
+            public void Start()
+            {
+                timer = new Stopwatch();
+                timer.Start();
+            }
+
+            public void Stop()
+            {
+                if (timer == null || !timer.IsRunning)
+                    return;
+
+                timer.Stop();
+                toolStripLabel.Text = $"{timer.ElapsedMilliseconds} ms";
+            }
+
+            public long ElapsedMiliseconds
+            {
+                get
+                {
+                    if (timer == null)
+                        return 0;
+
+                    return timer.ElapsedMilliseconds;
+                }
+            }
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            displayedIndex--;
+            DisplayImagesOfIndex();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            displayedIndex++;
+            DisplayImagesOfIndex();
+        }
     }
 }
